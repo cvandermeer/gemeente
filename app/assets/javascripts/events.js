c83d0ca5c6fc4fb238d1ed271f745813c77d1de8
Chris van der Meer <cvandermeer@live.nl>
2015-09-10 11:36:54 +0200

    also zooming when viewing location

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -6,8 +6,7 @@ ready = function() {
     var lon = parseFloat($(this).attr('data-lon'));
     var lat = parseFloat($(this).attr('data-lat'));
     view.setCenter(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'));
-    console.log($.type(lon));
-    console.log(lat);
+    view.setZoom(17);
   });
 
 }
