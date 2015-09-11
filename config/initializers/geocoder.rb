efa21ef90c5cd310c7254aa37487d60398e06c53
Chris van der Meer <cvandermeer@live.nl>
2015-09-11 10:53:43 +0200

    rubocop

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,15 +1,15 @@
 Geocoder.configure(
 
   # geocoding service (see below for supported options):
-  :lookup => :nominatim,
+  lookup: :nominatim,
 
   # IP address geocoding service (see below for supported options):
-  :ip_lookup => :maxmind,
+  ip_lookup: :maxmind,
 
   # geocoding service request timeout, in seconds (default 3):
-  :timeout => 5,
+  timeout: 5,
 
   # set default units to kilometers:
-  :units => :km,
+  unit: :km
 
 )
