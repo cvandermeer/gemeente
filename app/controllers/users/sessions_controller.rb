ecb0c2c987e3855323021ca12c04fd9e9c78354b
Chris van der Meer <cvandermeer@live.nl>
2015-09-21 14:35:49 +0200

    rubocop fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,5 @@
-class Users::SessionsController < Devise::SessionsController
-  # before_filter :configure_sign_in_params, only: [:create]
-  layout false
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   redirect_to root_path
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
+class Users
+  class SessionsController < Devise::SessionsController
+    ayout false
+  end
 end
