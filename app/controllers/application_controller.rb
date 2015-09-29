87f6e4f7bc033a26189047842eaa6deebff8de83
Chris van der Meer <cvandermeer@live.nl>
2015-09-29 13:07:05 +0200

    seeds for every role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if current_user.user?
+      reports_path
+    elsif current_user.community?
+      community_dashboard_path
+    elsif current_user.admin?
+      admin_dashboard_path
+    end
+  end
 end
