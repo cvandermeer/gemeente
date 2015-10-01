57eb47d165e6756fbced2b75ca45fd9039b898db
Chris van der Meer <cvandermeer@live.nl>
2015-10-01 15:08:28 +0200

    relation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   ### RELATIONS ###
-  has_one :community
+  belongs_to :community
 
   ### CONSTANTS ###
   ROLE_USER = 0
