5afc9c0e62fa31ba367bf8c939f8b670eab4f935
Chris van der Meer <cvandermeer@live.nl>
2015-10-01 12:49:51 +0200

    fixing rails best practices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  private
+
   def after_sign_in_path_for(_resource)
     if current_user.user?
       reports_path
