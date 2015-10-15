a1e1434e3227dc281ee1d3b18d014b51256dec72
Chris van der Meer <cvandermeer@live.nl>
2015-10-15 13:11:36 +0200

    Efficient improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'reports#dashboard'
-  resources :reports, except: [:show]
-  get 'reports/:id/delete', to: 'reports#delete', as: 'delete_report'
-  get '/community_dashboard', to: 'reports#community_dashboard'
-  get '/admin_dashboard', to: 'reports#admin_dashboard'
+  resources :reports, except: [:show] do
+    get :delete, on: :member
+    get :community_dashboard, on: :collection
+    get :admin_dashboard, on: :collection
+  end
 end
