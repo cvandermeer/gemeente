575d71835ee4da58cfc6115015ad3be6bfcd780c
Chris van der Meer <cvandermeer@live.nl>
2015-10-20 12:33:59 +0200

    using guard clause

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate_admin!
-    unless current_user.admin?
-      redirect_to root_path, alert: 'U bent niet gemachtigd!'
-    end
+    redirect_to root_path, alert: 'U bent niet gemachtigd!' unless current_user.admin?
   end
 end
