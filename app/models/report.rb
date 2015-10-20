15eecd23de512df4c51576408a387e39ab88ecdb
Bart Hoekstra <bart-hoekstra@live.nl>
2015-10-20 10:19:34 +0200

    Changed the method to something simpler

--- app/models/report.rb
+++ app/models/report.rb
@@ -17,39 +17,17 @@ class Report < ActiveRecord::Base
   validates :town, presence: true
 
   ### METHODS ###
-  def set_zipcode
-    @a = address
-    @b = address.split
-    @c = @b[@b.length - 1]
-    if @c.to_i.to_s == @c || @c.to_i >= 1 && @b.length > 1
-      @b.pop
-      @a = @b.join(' ')
-    end
-    self.zipcode = Zipcode.find_by(street: @a, town: town).zipcode unless @a.nil?
-  end
-
   def set_community
-    # Getting back the ascii
-    zipcode_ascii
-    # Find the community our create
-    @community_name = Zipcode.find_by(zipcode_ascii: @ascii.join).community
-    if !Community.find_by(name: @community_name).nil?
-      @community = Community.find_by(name: @community_name)
-    else
-      @community = Community.create(name: @community_name)
+    a = address
+    b = address.split
+    if b[b.length - 1].to_i >= 1
+      b.pop
+      a = b.join(' ')
     end
-    self.community_id = @community.id
-  end
-
-  def zipcode_ascii
-    @ascii = []
-    @zipcode_array = zipcode.split(//)
-    @zipcode_array.each do |z|
-      if z.to_i.to_s == z
-        @ascii.push(z)
-      else
-        @ascii.push(z.ord)
-      end
+    community_name = Zipcode.find_by(street: a, town: town).community unless a.nil?
+    if !Community.where(name: community_name).any?
+      Community.create(name: community_name)
     end
+    self.community = Community.find_by(name: community_name)
   end
 end
