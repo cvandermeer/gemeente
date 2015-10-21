327315946cf6c4dd5fa290f5defc647b606f4fe6
Chris van der Meer <cvandermeer@live.nl>
2015-10-21 10:47:04 +0200

    adding index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020130835) do
+ActiveRecord::Schema.define(version: 20151021084541) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151020130835) do
   end
 
   add_index "reports", ["community_id"], name: "index_reports_on_community_id", using: :btree
+  add_index "reports", ["user_id"], name: "index_reports_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
