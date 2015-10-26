9d7bb296e05b02c19652a75beb53c25f930b5fc5
Chris van der Meer <cvandermeer@live.nl>
2015-10-26 15:06:43 +0100

    trying figaro to store mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
-    port: 587,
-    enable_starttls_auto: true,
-    user_name: 'cvandermeer@live.nl',
-    password: 'RKDot8ZmZD9LV1-itPy7ng',
-    authentication: 'login',
-    domain: 'ikbeninwoner.nl'
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
   }
 end
