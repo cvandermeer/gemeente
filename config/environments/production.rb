f48b99b3b36e7dc55def867b56bbb52326a0c07f
Chris van der Meer <cvandermeer@live.nl>
2015-10-26 12:53:00 +0100

    multipart email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: 'smtp.mandrillapp.com',
     port: 25,
     enable_starttls_auto: true,
-    user_name: "cvandermeer@live.nl",
-    password: "RKDot8ZmZD9LV1-itPy7ng",
+    user_name: 'cvandermeer@live.nl',
+    password: 'RKDot8ZmZD9LV1-itPy7ng',
     authentication: 'login',
-    domain: 'ikbeninwoner.nl',
+    domain: 'ikbeninwoner.nl'
   }
 end
