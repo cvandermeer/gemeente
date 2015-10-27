46faafdc25fe90b373199f4a0d5eb09461034ad1
Bart Hoekstra <bart-hoekstra@live.nl>
2015-10-27 11:48:10 +0100

    rubocop fix]

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default to: proc.new { ['cvandermeer@live.nl', 'bart-hoekstra@live.nl'] },
-  from: 'info@ikbeninwoner.nl'
+          from: 'info@ikbeninwoner.nl'
   layout 'mailer'
 end
