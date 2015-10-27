9c1dd0a1bee3868d3962baab5ca98f81ab345bc4
Bart Hoekstra <bart-hoekstra@live.nl>
2015-10-27 13:53:54 +0100

    Added comments to the javascript files to explain the functions

--- app/assets/javascripts/notice.js
+++ app/assets/javascripts/notice.js
@@ -2,6 +2,11 @@ var ready;
 
 ready = function() {
 
+  /**
+    * @desc adds active class to notice and alert
+    * @return remove active class after 5 seconds
+  */
+
   if( $('.notice, .alert').length ) {
     $('.notice, .alert').addClass('active');
     setTimeout(function() {
