c6ee00784deabf58a3faf9cb2ebc3e3c7a76c62d
Chris van der Meer <cvandermeer@live.nl>
2015-10-28 13:32:07 +0100

    writing zipcode model tests

---
+++ test/models/zipcode_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ZipcodeTest < ActiveSupport::TestCase
+  test 'should get street search results' do
+    assert Zipcode.search_streets('Schoolpad').any?, 'No street search results'
+  end
+
+  test 'should get town search results' do
+    assert Zipcode.search_towns('Schoolpad').any?, 'No town search results'
+  end
+end
