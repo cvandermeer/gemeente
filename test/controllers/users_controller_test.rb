dd844b52de73b48c727ff3e70d65f534944cdf7a
Chris van der Meer <cvandermeer@live.nl>
2015-10-28 13:58:11 +0100

    user autentications tests

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  setup :initialize_user
+
+  def teardown
+    @user = nil
+  end
+
+  test 'user authentication error' do
+    assert_raises(Exception) {get :show, id: @user}
+  end
+
+  test 'user authentication no error' do
+    sign_in @user
+    get :show, id: @user
+    assert_response :success
+  end
+
+  private
+
+  def initialize_user
+    @user = users(:user1)
+  end
+end
