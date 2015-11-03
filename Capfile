fe1f83281b911b56a2859a04143e148765616c1a
Chris van der Meer <cvandermeer@live.nl>
2015-11-03 13:26:08 +0100

    adding sidekiq capistrano gem

--- Capfile
+++ Capfile
@@ -1,4 +1,6 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
 require 'capistrano/rails'
+require 'capistrano/sidekiq'
+require 'capistrano/sidekiq/monit'
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,13 @@ set :application, 'gemeente'
 set :repo_url, 'git@github.com:cvandermeer/gemeente.git'
 set :deploy_to, '/u/apps/gemeente_production'
 set :ssh_options, forward_agent: true
-set :pty, true
+set :pty, false
 set :linked_files, %w(config/database.yml .rbenv-vars .ruby-version)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 set :default_env, path: '/opt/rbenv/shims:$PATH'
 set :keep_releases, 5
 set :linked_dirs, fetch(:linked_dirs) + %w(public/system public/uploads)
+set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
 namespace :deploy do
   task :symlink_uploads do
     run "ln -nfs #{shared_path}/uploads  #{release_path}/public/uploads"
