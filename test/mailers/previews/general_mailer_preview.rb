8350cc34a2900f8c5657e77bed4955618f27341f
Chris van der Meer <cvandermeer@live.nl>
2015-11-12 11:53:37 +0100

    renaming and updating

--- test/mailers/previews/general_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/general_mailer
-class GeneralMailerPreview < ActionMailer::Preview
-  def send_community_notice_preview
-    GeneralMailer.new_community(Community.first)
-  end
-end
