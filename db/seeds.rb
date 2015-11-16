4cc060b01cdd7abf60d82874bf930854fb890fe5
Bart Hoekstra <bart-hoekstra@live.nl>
2015-11-16 10:17:04 +0100

    Seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create(email: 'inwoner@ikbeninwoner.nl', password: '123qweasd', role_id: 0)
-User.create(email: 'hollands-kroon@ikbeninwoner.nl', password: '123qweasd', role_id: 1, community_id: 1)
-User.create(email: 'admin@ikbeninwoner.nl', password: '123qweasd', role_id: 2)
 Community.create(name: 'Hollands Kroon')
+User.create(email: 'inwoner@ikbeninwoner.nl', password: '123qweasd', role_id: 0)
+User.create(email: 'hollands-kroon@ikbeninwoner.nl', password: '123qweasd', role_id: 1, community: Community.first)
+User.create(email: 'admin@ikbeninwoner.nl', password: '123qweasd', role_id: 2)
