f5011d8da2a6e6439fed8e2496b08fdf2ddbcdc7
Bart Hoekstra <bart-hoekstra@live.nl>
2015-11-24 12:52:51 +0100

    Added some panels

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin!, only: [:index]
 
-  def show
-    @user = User.find(params[:id])
+  def profile
+    @user = current_user
     @reports = @user.reports
-    @community_reports = @user.community.reports if @user.community?
+    @community_reports = @user.community.reports if @user.community
   end
 
   def index
