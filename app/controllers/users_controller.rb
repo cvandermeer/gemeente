9345f2e5732778ab4e893792eceb1f677f5557c4
Chris van der Meer <cvandermeer@live.nl>
2015-11-25 12:52:54 +0100

    users get a mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,6 @@ class UsersController < ApplicationController
   before_action :authenticate_admin!, only: [:index]
   layout false, only: [:new]
 
-  def profile
-    @user = current_user
-    @reports = @user.reports
-    @community_reports = @user.community.reports if @user.community
-  end
-
   def index
     @users = User.all
   end
@@ -21,6 +15,7 @@ class UsersController < ApplicationController
     @email = params[:user][:email]
     @generated_password = Devise.friendly_token.first(8)
     user = User.create!(email: @email, password: @generated_password)
+    GeneralMailer.generated_user(@email, @generated_password).deliver
     redirect_to admin_panel_path, notice: "Gebruiker #{@email} aangemaakt met wachtwoord #{@generated_password}"
   end
 end
