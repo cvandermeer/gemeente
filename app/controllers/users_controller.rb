3a1d8f8814ff9009058903388706beb1cae8379d
Chris van der Meer <cvandermeer@live.nl>
2015-11-26 11:24:29 +0100

    check for role before adding community

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,12 @@ class UsersController < ApplicationController
 
   def generate_user
     role_id = params[:user][:role_id]
+    community_id = nil
+    community_id = params[:user][:community_id] if role_id.to_i == 1
     @email = params[:user][:email]
     @generated_password = Devise.friendly_token.first(8)
-    user = User.create!(email: @email, password: @generated_password, role_id: role_id.to_i)
+    user = User.create!(email: @email, password: @generated_password, role_id: role_id.to_i, community_id: community_id)
     GeneralMailer.generated_user(@email, @generated_password).deliver
-    redirect_to admin_panel_path, notice: "Gebruiker #{@email} aangemaakt met wachtwoord #{@generated_password}"
+    redirect_to admin_users_path, notice: "Gebruiker #{@email} aangemaakt met wachtwoord #{@generated_password}"
   end
 end
