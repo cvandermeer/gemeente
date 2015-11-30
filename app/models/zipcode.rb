457069c7718c9154be17f79e5385a930ad431872
Bart Hoekstra <bart-hoekstra@live.nl>
2015-11-30 12:54:05 +0100

    Added the auto complete for the street and town when the users adds an report

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,18 +1,14 @@
 class Zipcode < ActiveRecord::Base
   ### METHODS ###
   def self.search_streets(search)
-    where('street like ?', "%#{search}%").map(&:street).uniq
-    # if streets.length < 5
-    #   streets += where('street like ?', "%#{search}%").map(&:street).uniq
-    # end
-    # streets.uniq[0..4]
+    streets = where('street like ?', "#{search}")
+    streets += where('street like ?', "%#{search}%")
+    streets.map(&:street).uniq
   end
 
   def self.search_towns(search)
-    towns = where('town like ?', "#{search}").map(&:town).uniq
-    if towns.length < 5
-      towns += where('town like ?', "%#{search}%").map(&:town).uniq
-    end
-    towns.uniq[0..4]
+    towns = where('town like ?', "#{search}")
+    towns += where('town like ?', "%#{search}%")
+    towns.map(&:town).uniq
   end
 end
