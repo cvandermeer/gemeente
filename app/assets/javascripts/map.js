ad5c1ebc46af4e61526f2c5fd5b63dbaa8586e06
Bart Hoekstra <bart-hoekstra@live.nl>
2015-12-11 12:25:51 +0100

    Setup landings page

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,95 +1,3 @@
-var mapStyles = [ {
-  featureType:'road',
-  elementType:'labels',
-  stylers:[
-    {visibility:'simplified'},
-    {lightness:20}
-  ]}, {
-  featureType:'administrative.land_parcel',
-  elementType:'all',
-  stylers:[
-    {visibility:'off'}
-  ]},{
-  featureType:'landscape.man_made',
-  elementType:'all',
-  stylers:[
-    {visibility:'on'}
-  ]},{
-  featureType:'transit',
-  elementType:'all',
-  stylers:[
-    {saturation:-100},
-    {visibility:'on'},
-    {lightness:10}
-  ]},{
-  featureType:'road.local',
-  elementType:'all',
-  stylers:[
-    {visibility:'on'}
-  ]},{
-  featureType:'road.local',
-  elementType:'all',
-  stylers:[
-    {visibility:'on'}
-  ]},{
-  featureType:'road.highway',
-  elementType:'labels',
-  stylers:[
-    {visibility:'simplified'}
-  ]},{
-  featureType:'poi',
-  elementType:'labels',
-  stylers:[
-    {visibility:'off'}
-  ]},{
-  featureType:'road.arterial',
-  elementType:'labels',
-  stylers:[
-    {visibility:'on'},
-    {lightness:50}
-  ]},{
-  featureType:'water',
-  elementType:'all',
-  stylers:[
-    {hue:'#a1cdfc'},
-    {saturation:30},
-    {lightness:49}
-  ]},{
-  featureType:'road.highway',
-  elementType:'geometry',
-  stylers:[
-    {hue:'#f49935'}
-  ]},{
-  featureType:'road.arterial',
-  elementType:'geometry',
-  stylers:[
-    {hue:'#fad959'}
-  ]}, {
-  featureType:'road.highway',
-  elementType:'all',
-  stylers:[
-    {hue:'#dddbd7'},
-    {saturation:-92},
-    {lightness:60},
-    {visibility:'on'}
-  ]}, {
-  featureType:'landscape.natural',
-  elementType:'all',
-  stylers:[
-    {hue:'#c8c6c3'},
-    {saturation:-51},
-    {lightness:-5},
-    {visibility:'on'}
-  ]}, {
-  featureType:'poi',
-  elementType:'all',
-  stylers:[
-    {hue:'#d9d5cd'},
-    {saturation:-70},
-    {lightness:20},
-    {visibility:'on'}
-  ]}
-];
 var ready;
 var map;
 
@@ -111,7 +19,7 @@ var report_show_position;
 function initMap() {
   if ($('#map').length) {
     pos = {lat: 52.397, lng: 5.544};
-    zoom = 8;
+    zoom = 9;
 
     if($('.community-data').length){
       pos = {lat: parseFloat($('.community-data').attr('data-lat')), lng: parseFloat($('.community-data').attr('data-lon'))};
@@ -121,20 +29,19 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
       zoom: zoom,
       center: pos,
-      styles: mapStyles,
       disableDefaultUI: true,
       streetViewControl: true,
       streetViewControlOptions: {
-        position: google.maps.ControlPosition.LEFT_TOP
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
       },
-      scrollwheel: false,
+      minZoom: 9,
       zoomControl: true,
       zoomControlOptions: {
-         position: google.maps.ControlPosition.LEFT_TOP
+         position: google.maps.ControlPosition.RIGHT_BOTTOM
       },
       mapTypeControl: true,
       mapTypeControlOptions: {
-        position: google.maps.ControlPosition.TOP_RIGHT
+        position: google.maps.ControlPosition.LEFT_BOTTOM
       },
       draggable: true
     });
