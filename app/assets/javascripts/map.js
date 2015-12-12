5b11b5a8949b45483b4d990876d245c78a3861e9
Bart Hoekstra <bart-hoekstra@live.nl>
2015-12-12 14:54:22 +0100

    Added the search bar, and a option to only search in the netherlands

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -141,51 +141,26 @@ function resetMap() {
 function setSearchBar(map) {
   // Search bar with autocomplete
   var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
+  var options = {
+    //types: ['(regions)'],
+    componentRestrictions: {country: 'nl'}
+  };
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+  autocomplete.bindTo('bounds', map);
 
-  var markers = [];
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-    if ( places.length === 0 ) {
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
       return;
     }
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(10);
+    }
   });
 }
 
