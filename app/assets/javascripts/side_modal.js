fa8af9702d224f8a1d609e368c96b975e5da4aa1
Bart Hoekstra <bart-hoekstra@live.nl>
2015-12-24 10:53:09 +0100

    Resets the height of all_reports, when you remove a report

--- app/assets/javascripts/side_modal.js
+++ app/assets/javascripts/side_modal.js
@@ -27,7 +27,6 @@ function bindSideModalHandlers() {
     removeSideModal($('.side-modal'));
   });
   $('.modal-confirm.yes').bind('ajax:success', function(e, data, status) {
-    //console.log(data)
     removeReportAndMarker(data);
   });
 }
@@ -35,4 +34,14 @@ function bindSideModalHandlers() {
 function removeReportAndMarker(data) {
   $('.report-show[data-report-id="'+data.id+'"]').remove();
   $('.marker[data-marker-id="'+data.id+'"]').parent().remove();
+  resetHeightOfAllReports();
+}
+
+function resetHeightOfAllReports() {
+  var report_count = $('.js_all_reports').find('.report-show').length;
+  if(report_count > 6) {
+    $('.js_all_reports').height(88 * 6 + 44);
+  } else {
+    $('.js_all_reports').height(88 * report_count - 1);
+  }
 }
