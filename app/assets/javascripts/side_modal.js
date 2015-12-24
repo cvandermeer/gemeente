5f4b2af84de139283a8bf260b29e220d2c5b036a
Bart Hoekstra <bart-hoekstra@live.nl>
2015-12-24 09:45:18 +0100

    Added the remove side modal handler

--- app/assets/javascripts/side_modal.js
+++ app/assets/javascripts/side_modal.js
@@ -1,13 +1,30 @@
 function initSideModal() {
   $('.js_side_modal').bind('ajax:success', function(e, data, status) {
     setDataToSideModal(data);
-    console.log(data);
+    //console.log(data);
   });
 }
 
 function setDataToSideModal(data) {
+  if($('.side-modal').length !== 0) {
+    removeSideModal($('.side-modal'));
+  }
   $('body').append(data);
   setTimeout(function(){
     $('.side-modal').addClass('active');
   }, 300);
+  bindSideModalHandlers();
+}
+
+function removeSideModal(modal) {
+  modal.removeClass('active');
+  setTimeout(function() {
+    modal.remove();
+  }, 300);
+}
+
+function bindSideModalHandlers() {
+  $('.js_close_modal').on('click', function() {
+    removeSideModal($('.side-modal'));
+  });
 }
