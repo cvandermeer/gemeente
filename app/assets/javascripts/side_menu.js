9574359a0949c0e048cc00ad144052ed88579c0d
Bart Hoekstra <bart-hoekstra@live.nl>
2016-01-09 09:11:35 +0100

    Changed some styling for the menu

--- app/assets/javascripts/side_menu.js
+++ app/assets/javascripts/side_menu.js
@@ -5,7 +5,7 @@ ready = function() {
   // toggles class to show the side menu
   $('.hamburger').on('click', function() {
     $('.hamburger').toggleClass('active');
-    $('.index-outer-wrapper, .outer-wrapper, .side-menu').toggleClass('show-side-menu');
+    $('header, .index-outer-wrapper, .outer-wrapper, .side-menu').toggleClass('show-side-menu');
   });
 
   // removes class to hide side menu if active
@@ -13,7 +13,7 @@ ready = function() {
     if ($('.hamburger.active').length) {
       e.preventDefault();
       $('.hamburger').removeClass('active');
-      $('.index-outer-wrapper, .outer-wrapper, .side-menu').removeClass('show-side-menu');
+      $('header, .index-outer-wrapper, .outer-wrapper, .side-menu').removeClass('show-side-menu');
     }
   });
 };
