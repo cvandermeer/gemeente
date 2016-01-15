e94b48c73bc3075e5916415bde14a96b7369a569
Bart Hoekstra <bart-hoekstra@live.nl>
2016-01-15 10:29:13 +0100

    Added comments to the communities.js

--- app/assets/javascripts/communities.js
+++ app/assets/javascripts/communities.js
@@ -1,5 +1,11 @@
 var ready;
 
+/**
+ * Sets the value of the Select2 field on page newsletters
+ * and changes the location of the url with the id of the community
+ * @return {boolean}
+ */
+
 ready = function() {
   $('#gemeente').on('change', function () {
      var url = "/communities/" + $(this).val() + '/news';
