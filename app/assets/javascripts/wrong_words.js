76ad579e76a19148044b768a4a9f3a20e8873231
Bart Hoekstra <bart-hoekstra@live.nl>
2016-01-16 11:16:10 +0100

    Append new word to the right position

--- app/assets/javascripts/wrong_words.js
+++ app/assets/javascripts/wrong_words.js
@@ -21,16 +21,29 @@ function newWrongWordRepsonse() {
   $('.new_wrong_word').on('ajax:success', function(e, data, status) {
     if (data.status === 200) {
       // Data is succesfully posted
-      console.log(data);
+      setWrongWordData(data.wrong_word);
     } else if (data.status === 422) {
       // Could not proces the data
-      validate.setErrorMessage(0, $('.js_word').val() + ' staat al in de database');
-      validate.setFocus(0);
-      validate.setValid(0, false);
-      validate.setClass(0);
+      setValidationForWrongWordIfInDataBase();
     }
   });
 }
+
+function setValidationForWrongWordIfInDataBase() {
+  validate.setErrorMessage(0, $('.js_word').val() + ' staat al in de database');
+  validate.setValid(0, false);
+  validate.setClass(0);
+}
+
+function setWrongWordData(data) {
+  var wrong_word_position = setWrongWordPosition(data.word);
+  wrong_word_position.find('ul').append('<li>' + data.word + '</li>');
+}
+
+function setWrongWordPosition(data) {
+  return $('.letter[data-letter="'+data.split('')[0]+'"]');
+}
+
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
