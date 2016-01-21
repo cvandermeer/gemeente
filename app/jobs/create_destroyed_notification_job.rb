b6794f3c6a359131bfd51180e861730249e51caf
Bart Hoekstra <bart-hoekstra@live.nl>
2016-01-21 21:43:00 +0100

    Update create_destroyed_notification_job.rb

--- app/jobs/create_destroyed_notification_job.rb
+++ app/jobs/create_destroyed_notification_job.rb
@@ -2,7 +2,7 @@ class CreateDestroyedNotificationJob < ActiveJob::Base
   queue_as :default
 
   def perform(report)
-    Notification.create(title: "Uw melding: #{report.category.title} is verwijdert",
+    Notification.create(title: "Uw melding: #{report.category.title} is verwijderd",
                         user_id: report.user_id,
                         category_id: Notification::CATEGORY_RECORD_DESTROYED,
                         record_id: report.id)
